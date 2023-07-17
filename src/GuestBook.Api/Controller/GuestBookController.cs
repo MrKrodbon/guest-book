dd9a3107ba293f83c74e1491644d019195ce8201
eugene <sichi331@gmail.com>
2023-07-17 21:39:47 +0300

    TD-1 Created solution structure

---
+++ src/GuestBook.Api/Controller/GuestBookController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GuestBook.Api.Controller;
+public class GuestBookController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
